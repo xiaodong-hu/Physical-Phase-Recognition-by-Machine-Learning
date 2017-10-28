fc97b3879950208867064c3b85c95f5b1a6a80bc
Xiaodong Hu <cdqz2014@mail.ustc.edu.cn>
2017-10-28 16:57:01 +0800

    Automatic and parallel data generation. Still some bugs on paralleling generating and moving files

--- Ising Model Data/data_generator.py
+++ Ising Model Data/data_generator.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import multiprocessing as mp
 
 #if os.path.exists('/home/hxd/Documents/数据挖掘导论课题-Phase Recognition by Machine Learning/Ising Model Data/data'):
 if os.path.exists('data'):
@@ -17,10 +18,17 @@ number = input('Input the number of configuration you want to generate:\n')
 
 delta_T = ((float)(temperature_high)-(float)(temperature_low))/(float)(number)
 
-print('\n')
-for i in range(int(number)):
+pool = mp.Pool(8)
+
+def run(i):
     T = str((float)(temperature_low) + (float)(i)*delta_T)
     os.system('./Ising'+' -n '+size+' -c '+cycle+' -t '+T)
-    os.system('mv output.csv'+' '+'data/output'+str(i)+'.csv')
     if (i+1)%50 == 0:
         print('{}\tdata has been generated!'.format(i+1))
+    os.system('mv output.csv'+' '+'data/output'+str(i)+'.csv')
+
+pool.map(run,range(int(number)))
+
+#def move_to_data(i):
+
+#pool.map(move_to_data,range(int(number)))
