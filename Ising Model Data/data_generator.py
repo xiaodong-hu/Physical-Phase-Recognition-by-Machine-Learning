551df4f9d74b43369fd943f8e0bfe5c2683a65fa
Xiaodong Hu <cdqz2014@mail.ustc.edu.cn>
2017-11-06 12:00:46 +0800

    training demo built (no paralleling)

--- Ising Model Data/data_generator.py
+++ Ising Model Data/data_generator.py
@@ -2,33 +2,21 @@ import os
 import shutil
 import multiprocessing as mp
 
-#if os.path.exists('/home/hxd/Documents/数据挖掘导论课题-Phase Recognition by Machine Learning/Ising Model Data/data'):
-if os.path.exists('data'):
-    if input('directory already exits. Are you sure to cover it? (yes|no)\n')=='yes':
-        shutil.rmtree('data')
+def makedir():
+    if os.path.exists('data'):
+        if input('directory already exits. Are you sure to cover it? (yes|no)\n')=='yes':
+            shutil.rmtree('data')
+            os.mkdir('data')
+    else:
         os.mkdir('data')
-else:
-    os.mkdir('data')
-
-
-os.system('cp Ising data/Ising')
-work_directory = os.getcwd()
-os.chdir(work_directory+'/data') # change directory to /data
-
-size = input('Input the size of lattice:(default is 10)\n')
-cycle = input('Input the time reaching equilibrium:(default is 2000)\n')
-temperature_low = input('Input the starting low temperature:')
-temperature_high = input('Input the final high temperature:')
-number = input('Input the number of configuration you want to generate:\n')
-
-delta_T = ((float)(temperature_high)-(float)(temperature_low))/(float)(number)
 
 def run(i):
     T = str((float)(temperature_low) + (float)(i)*delta_T)
     output_order = str(i)
     os.system('./Ising'+' -n '+size+' -c '+cycle+' -t '+T+' -o '+output_order)
-    #if (i+1)%50 == 0:
-    #    print('{}\tdata has been generated!'.format(i+1))
+    i = len(os.listdir())
+    if (i%50) == 0:
+        print('{}\tdata has been generated!'.format(i))
 
 def move(i):
     order = str(i);
@@ -36,6 +24,21 @@ def move(i):
 
 
 if __name__ == "__main__":
+    
+    makedir()
+    os.system('cp Ising data/Ising')
+    work_directory = os.getcwd()
+    os.chdir(work_directory+'/data') # change directory to /data
+
+    size = input('Input the size of lattice:(default is 10)\n')
+    cycle = input('Input the time reaching equilibrium:(default is 2000)\n')
+    temperature_low = input('Input the starting low temperature:')
+    temperature_high = input('Input the final high temperature:')
+    number = input('Input the number of configuration you want to generate:\n')
+    print('\n\n')
+    
+    delta_T = ((float)(temperature_high)-(float)(temperature_low))/(float)(number)
+    
     pool = mp.Pool(processes=8)
     pool.map(run,range(int(number)))
     pool.map(move,range(int(number)))
